206b9de89b15205b28857e56214712b591b18e91
Sandali <sanwije2@gmail.com>
2021-02-25 10:23:40 +0530

    Initial Commit

---
+++ src/DB/DbConnection.java
@@ -0,0 +1,24 @@
+package DB;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+	public static Connection connect() {
+		Connection con = null;
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:TimeTable.db");//connecting to our database
+		System.out.println("Connected");
+		} catch (ClassNotFoundException| SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e+"");
+		}
+		
+		return con;
+	}
+
+}
