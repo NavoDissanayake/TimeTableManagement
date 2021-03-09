72a3841dfee38b9f1f75f844948fd42d6d271d5c
IT19207346 <63913339+IT19207346@users.noreply.github.com>
2021-03-09 08:07:56 +0530

    Completed Interfaces

---
+++ src/WorkingDays/NotAvailableTime.java
@@ -0,0 +1,465 @@
+package WorkingDays;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
+
+import Lecturer.Add_Lecturer;
+import Student.Manage_studentGroup;
+import javax.swing.JRadioButton;
+
+public class NotAvailableTime {
+
+	private Image home_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/home.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image lec_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/lecturer.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image stu_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/student.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image sub_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/subject.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image session_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/session.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image tag_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/tags.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image location_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/location.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image st_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/statics.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image days_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/Wdays.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image time_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/time.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image adv_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/adv1.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image room_logo = new ImageIcon(Manage_studentGroup.class.getResource("/images/room.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	
+	
+	public JFrame frame;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NotAvailableTime window = new NotAvailableTime();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public NotAvailableTime() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * @wbp.parser.entryPoint
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
+		frame.setBackground(Color.YELLOW);
+		frame.setResizable(false);
+		frame.setTitle("Student");
+		frame.setSize(1350, 728);
+		frame.setBounds(0, 0, 1350, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(Color.WHITE);
+		panel.setBounds(0, 0, 1344, 65);
+		panel.setBackground(new Color(0, 139, 139));
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_10 = new JLabel("Working Days And Hours");
+		lblNewLabel_10.setForeground(Color.WHITE);
+		lblNewLabel_10.setFont(new Font("Times New Roman", Font.BOLD, 27));
+		lblNewLabel_10.setBackground(Color.WHITE);
+		lblNewLabel_10.setBounds(723, 13, 419, 29);
+		panel.add(lblNewLabel_10);
+		/*
+		 * //JLabel lblNewLabel = new JLabel("Time Table Management System");
+		 * lblNewLabel.setBounds(261, 5, 822, 61); panel.add(lblNewLabel);
+		 * lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 50));
+		 * lblNewLabel.setForeground(Color.WHITE);
+		 */
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 62, 262, 609);
+		panel_1.setBackground(new Color(230, 230, 250));
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+	
+		JButton btnLecturers = new JButton("Lecturers");
+		btnLecturers.setHorizontalAlignment(SwingConstants.LEFT);
+		btnLecturers.setIcon(new ImageIcon(lec_logo));
+		btnLecturers.addActionListener(new ActionListener() {
+				
+				public void actionPerformed(ActionEvent e) {
+					
+					Add_Lecturer add_lecture = new Add_Lecturer();
+					add_lecture.main(null);
+					frame.dispose();
+					
+				}
+			});
+		btnLecturers.setForeground(Color.WHITE);
+		btnLecturers.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnLecturers.setBackground(new Color(0, 139, 139));
+		btnLecturers.setBounds(0, 59, 264, 38);
+		panel_1.add(btnLecturers);
+		
+		JButton btnStudents = new JButton("Students");
+		btnStudents.setHorizontalAlignment(SwingConstants.LEFT);
+		btnStudents.setIcon(new ImageIcon(stu_logo));
+		btnLecturers.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				Manage_studentGroup add_st = new Manage_studentGroup();
+				add_st.main(null);
+				frame.dispose();
+				
+			}
+		});
+		btnStudents.setForeground(Color.WHITE);
+		btnStudents.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnStudents.setBackground(new Color(0, 139, 139));
+		btnStudents.setBounds(0, 108, 264, 38);
+		panel_1.add(btnStudents);
+		
+		JButton btnSubjects = new JButton("Subjects");
+		btnSubjects.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSubjects.setIcon(new ImageIcon(sub_logo));
+		btnSubjects.setForeground(Color.WHITE);
+		btnSubjects.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnSubjects.setBackground(new Color(0, 139, 139));
+		btnSubjects.setBounds(0, 157, 264, 38);
+		panel_1.add(btnSubjects);
+		
+		JButton btnSessions = new JButton("Sessions");
+		btnSessions.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSessions.setIcon(new ImageIcon(session_logo));
+		btnSessions.setForeground(Color.WHITE);
+		btnSessions.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnSessions.setBackground(new Color(0, 139, 139));
+		btnSessions.setBounds(0, 206, 264, 38);
+		panel_1.add(btnSessions);
+		
+		JButton btnTags = new JButton("Tags");
+		btnTags.setHorizontalAlignment(SwingConstants.LEFT);
+		btnTags.setIcon(new ImageIcon(tag_logo));
+		btnTags.setForeground(Color.WHITE);
+		btnTags.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnTags.setBackground(new Color(0, 139, 139));
+		btnTags.setBounds(0, 255, 264, 38);
+		panel_1.add(btnTags);
+		
+		JButton btnRooms = new JButton("Rooms");
+		btnRooms.setHorizontalAlignment(SwingConstants.LEFT);
+		btnRooms.setIcon(new ImageIcon(room_logo));
+		btnRooms.setForeground(Color.WHITE);
+		btnRooms.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnRooms.setBackground(new Color(0, 139, 139));
+		btnRooms.setBounds(0, 304, 264, 38);
+		panel_1.add(btnRooms);
+		
+		JButton btnWorkingDays = new JButton("Working days & Hours");
+		btnWorkingDays.setHorizontalAlignment(SwingConstants.LEFT);
+		btnWorkingDays.setIcon(new ImageIcon(days_logo));
+		btnWorkingDays.setForeground(Color.WHITE);
+		btnWorkingDays.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnWorkingDays.setBackground(new Color(0, 139, 139));
+		btnWorkingDays.setBounds(0, 353, 264, 38);
+		panel_1.add(btnWorkingDays);
+		
+		JButton btnLocations = new JButton("Locations");
+		btnLocations.setHorizontalAlignment(SwingConstants.LEFT);
+		btnLocations.setIcon(new ImageIcon(location_logo));
+		btnLocations.setForeground(Color.WHITE);
+		btnLocations.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnLocations.setBackground(new Color(0, 139, 139));
+		btnLocations.setBounds(0, 402, 264, 38);
+		panel_1.add(btnLocations);
+		
+		JButton btnStatistics = new JButton("Statistics");
+		btnStatistics.setHorizontalAlignment(SwingConstants.LEFT);
+		btnStatistics.setIcon(new ImageIcon(st_logo));
+		btnStatistics.setForeground(Color.WHITE);
+		btnStatistics.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnStatistics.setBackground(new Color(0, 139, 139));
+		btnStatistics.setBounds(0, 451, 264, 38);
+		panel_1.add(btnStatistics);
+		
+		JButton btnAdvanced = new JButton("Advanced");
+		btnAdvanced.setHorizontalAlignment(SwingConstants.LEFT);
+		btnAdvanced.setIcon(new ImageIcon(adv_logo));
+		btnAdvanced.setForeground(Color.WHITE);
+		btnAdvanced.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnAdvanced.setBackground(new Color(0, 139, 139));
+		btnAdvanced.setBounds(0, 500, 264, 38);
+		panel_1.add(btnAdvanced);
+		
+		JButton btnTimetableGenerate = new JButton("Timetable Generate");
+		btnTimetableGenerate.setHorizontalAlignment(SwingConstants.LEFT);
+		btnTimetableGenerate.setIcon(new ImageIcon(time_logo));
+		btnTimetableGenerate.setForeground(Color.WHITE);
+		btnTimetableGenerate.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnTimetableGenerate.setBackground(new Color(0, 139, 139));
+		btnTimetableGenerate.setBounds(0, 549, 264, 38);
+		panel_1.add(btnTimetableGenerate);
+		
+		
+		JButton btnManageGroups = new JButton("Home");
+		btnManageGroups.setHorizontalAlignment(SwingConstants.LEFT);
+		btnManageGroups.setIcon(new ImageIcon(home_logo));
+		btnManageGroups.setBounds(0, 13, 262, 33);
+		panel_1.add(btnManageGroups);
+		btnManageGroups.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+		btnManageGroups.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnManageGroups.setForeground(new Color(255, 255, 255));
+		btnManageGroups.setBackground(new Color(0, 139, 139));
+		btnManageGroups.setFont(new Font("Tahoma", Font.BOLD, 17));
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBackground(new Color(230, 230, 250));
+		panel_3.setBounds(263, 62, 1081, 628);
+		frame.getContentPane().add(panel_3);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setLayout(null);
+		panel_6.setBounds(10, 50, 1082, 49);
+		panel_3.add(panel_6);
+		
+		JLabel lblNewLabel_1 = new JLabel("Not Available Time Manage");
+		lblNewLabel_1.setForeground(new Color(0, 128, 128));
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(406, 13, 436, 31);
+		panel_6.add(lblNewLabel_1);
+		
+		JButton btnNewButton_2 = new JButton("Add Working Days");
+		btnNewButton_2.setBounds(0, 2, 258, 37);
+		panel_3.add(btnNewButton_2);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				AddWorkingdays m_stgroup = new AddWorkingdays ();
+				m_stgroup.main(null);
+				frame.dispose();
+				
+			}
+		});
+		
+		
+		JButton btnNewButton_2_1 = new JButton("Manage Working Days");
+		btnNewButton_2_1.setBounds(255, 2, 258, 37);
+		panel_3.add(btnNewButton_2_1);
+	btnNewButton_2_1.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				ManageWorkingDays m_stgroup = new ManageWorkingDays ();
+				m_stgroup.main(null);
+				frame.dispose();
+				
+			}
+		});
+		
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setLayout(null);
+		panel_4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_4.setBackground(SystemColor.menu);
+		panel_4.setBounds(92, 124, 853, 315);
+		panel_3.add(panel_4);
+		
+		JLabel lblNewLabel_2 = new JLabel("Select Lecturer");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(106, 11, 91, 23);
+		panel_4.add(lblNewLabel_2);
+		
+		JButton btnDelete = new JButton("ADD");
+		btnDelete.setBounds(671, 57, 141, 31);
+		panel_4.add(btnDelete);
+		btnDelete.setForeground(Color.WHITE);
+		btnDelete.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnDelete.setEnabled(true);
+		btnDelete.setBackground(new Color(0, 153, 153));
+		
+		JButton btnClear = new JButton("CLEAR");
+		btnClear.setBounds(671, 130, 141, 31);
+		panel_4.add(btnClear);
+		btnClear.setForeground(Color.WHITE);
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnClear.setEnabled(true);
+		btnClear.setBackground(new Color(0, 153, 153));
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(279, 12, 149, 22);
+		panel_4.add(comboBox);
+		
+		JLabel lblSelectGroup = new JLabel("Select Group");
+		lblSelectGroup.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSelectGroup.setBounds(106, 62, 91, 23);
+		panel_4.add(lblSelectGroup);
+		
+		JLabel lblSelectSubGroup = new JLabel("Select Sub Group");
+		lblSelectSubGroup.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSelectSubGroup.setBounds(106, 119, 113, 23);
+		panel_4.add(lblSelectSubGroup);
+		
+		JLabel lblSelectSessionId = new JLabel("Select Session ID");
+		lblSelectSessionId.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSelectSessionId.setBounds(106, 181, 113, 23);
+		panel_4.add(lblSelectSessionId);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(279, 63, 149, 22);
+		panel_4.add(comboBox_1);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(279, 120, 149, 22);
+		panel_4.add(comboBox_2);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setBounds(279, 182, 149, 22);
+		panel_4.add(comboBox_3);
+		
+		JLabel lblStartTime = new JLabel("Start Time");
+		lblStartTime.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblStartTime.setBounds(106, 243, 91, 23);
+		panel_4.add(lblStartTime);
+		
+		JLabel lblEndTime = new JLabel("End Time");
+		lblEndTime.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblEndTime.setBounds(398, 243, 57, 23);
+		panel_4.add(lblEndTime);
+		
+		textField = new JTextField();
+		textField.setBounds(207, 245, 86, 20);
+		panel_4.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(478, 245, 86, 20);
+		panel_4.add(textField_1);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("AM");
+		rdbtnNewRadioButton.setBounds(299, 244, 57, 23);
+		panel_4.add(rdbtnNewRadioButton);
+		
+		JRadioButton radioButton = new JRadioButton("AM");
+		radioButton.setBounds(570, 244, 57, 23);
+		panel_4.add(radioButton);
+		
+		JRadioButton rdbtnPm = new JRadioButton("PM");
+		rdbtnPm.setBounds(299, 270, 57, 23);
+		panel_4.add(rdbtnPm);
+		
+		JRadioButton rdbtnPm_1 = new JRadioButton("PM");
+		rdbtnPm_1.setBounds(570, 270, 57, 23);
+		panel_4.add(rdbtnPm_1);
+		
+		JButton btnNewButton = new JButton("DELETE");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton.setEnabled(true);
+		btnNewButton.setBackground(new Color(0, 153, 153));
+		btnNewButton.setBounds(766, 567, 141, 31);
+		panel_3.add(btnNewButton);
+	btnNewButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				ManageWorkingDays m_stgroup = new ManageWorkingDays ();
+				m_stgroup.main(null);
+				frame.dispose();
+				
+			}
+		});
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(32, 178, 170));
+		separator.setBackground(new Color(0, 139, 139));
+		separator.setBounds(0, 40, 1082, 31);
+		panel_3.add(separator);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(92, 450, 852, 111);
+		panel_3.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton button = new JButton("Not available time");
+		button.setBounds(511, 2, 258, 37);
+		panel_3.add(button);
+		
+		
+		
+		JButton button_1 = new JButton("Add Not Available Time");
+		button_1.setBounds(766, 2, 258, 37);
+		panel_3.add(button_1);
+		
+	button_1.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				NotAvailableLocation m_stgroup = new NotAvailableLocation ();
+				m_stgroup.main(null);
+				frame.dispose();
+				
+			}
+		});
+		
+		JButton btnEdit = new JButton("EDIT");
+		btnEdit.setForeground(Color.WHITE);
+		btnEdit.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnEdit.setEnabled(true);
+		btnEdit.setBackground(new Color(0, 153, 153));
+		btnEdit.setBounds(102, 567, 141, 31);
+		panel_3.add(btnEdit);
+		
+		JButton btnUpdate = new JButton("UPDATE");
+		btnUpdate.setForeground(Color.WHITE);
+		btnUpdate.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnUpdate.setEnabled(true);
+		btnUpdate.setBackground(new Color(0, 153, 153));
+		btnUpdate.setBounds(290, 567, 141, 31);
+		panel_3.add(btnUpdate);
+	}
+}
